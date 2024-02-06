cc7032f969ee902c81e0dbda501cada414363ae9
Tomek Turba <tturba@gmail.com>
2024-02-06 02:33:54 +0100

    Update voip_gen.py

--- voip_gen.py
+++ voip_gen.py
@@ -1,24 +1,18 @@
 from scapy.all import *
 from scapy.layers.inet import IP, UDP
 from scapy.layers.rtp import RTP
-from scapy.layers.sip import SIP
 
 packets = []
 
-# Symulacja inicjacji połączenia SIP
-sip_methods = ["INVITE", "TRYING", "RINGING", "OK"]
-for method in sip_methods:
-    packets.append(IP(dst="192.168.1.1") / UDP(sport=5060, dport=5060) / SIP(Method=method))
+# Destination IP address and ports
+destination_ip = "192.168.1.2"
+source_port = 1234
+destination_port = 1234
 
-# Symulacja danych RTP
-for i in range(100):  # Generowanie 100 pakietów RTP
-    rtp_packet = IP(dst="192.168.1.2") / UDP(sport=1234, dport=1234) / RTP(version=2, payload_type=0, sequence=10000+i, timestamp=3000*i)
+# Generating 100 RTP packets
+for i in range(100):
+    rtp_packet = IP(dst=destination_ip) / UDP(sport=source_port, dport=destination_port) / RTP(version=2, payload_type=0, sequence=10000+i, timestamp=3000*i)
     packets.append(rtp_packet)
 
-# Symulacja zakończenia połączenia SIP
-sip_end_methods = ["BYE", "OK"]
-for method in sip_end_methods:
-    packets.append(IP(dst="192.168.1.1") / UDP(sport=5060, dport=5060) / SIP(Method=method))
-
-# Zapisanie pakietów do pliku PCAP
-wrpcap("voip_simulation_extended.pcap", packets)
+# Saving the packets to a PCAP file
+wrpcap("rtp_simulation.pcap", packets)
